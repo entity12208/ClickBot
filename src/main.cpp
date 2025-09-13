c5a8976d25189fa8ad893fe1777b5c78a9cde8a0
entity12208 <116198380+entity12208@users.noreply.github.com>
2025-09-13 06:49:04 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,111 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
-
-#ifdef GEODE_IS_WINDOWS
-#include <Windows.h>
-#endif
+#include <Geode/binding/PlayLayer.hpp>
+#include <Geode/binding/PlayerObject.hpp>
+#include <Geode/binding/GameManager.hpp>
+#include <Geode/ui/TextInput.hpp>
+#include <Geode/binding/FLAlertLayer.hpp>
 
 using namespace geode::prelude;
 
-// --- Global state variable for the clickbot ---
-bool g_clicking = false;
+static int g_holdTimeMs = 100;
+static int g_releaseTimeMs = 100;
+static bool g_clicking = false;
+static bool g_popupOpen = false;
+static float g_timer = 0.f;
+static bool g_pressed = false;
 
-// --- Click thread function ---
-// This function runs in a separate thread and simulates key presses
-// based on the global state of the clickbot.
-DWORD WINAPI clickThread(LPVOID) {
-    // Loop indefinitely to check the clickbot state
-    while (true) {
-        // Retrieve the current settings from the mod
-        // These values are automatically loaded and saved by Geode's settings system.
-        auto mod = Mod::get();
-        int holdTimeMs = mod->getSettingValue<int>("hold-time");
-        int releaseTimeMs = mod->getSettingValue<int>("release-time");
+class TimeEditPopup : public FLAlertLayer, public TextInputDelegate {
+protected:
+    TextInput* m_holdInput = nullptr;
+    TextInput* m_releaseInput = nullptr;
 
-        // Only run if the clickbot is enabled
-        if (g_clicking) {
-            // Press the space key
-            keybd_event(VK_SPACE, 0, 0, 0);
-            Sleep(holdTimeMs);
-
-            // Release the space key
-            keybd_event(VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
-            Sleep(releaseTimeMs);
-        } else {
-            // Wait for a short period to avoid high CPU usage
-            Sleep(50);
+public:
+    static TimeEditPopup* create() {
+        auto ret = new TimeEditPopup();
+        if (ret && ret->init(nullptr, "Clickbot Settings", "OK", "Cancel", 300.f, true, 300.f, 200.f)) {
+            ret->autorelease();
+            return ret;
         }
+        CC_SAFE_DELETE(ret);
+        return nullptr;
     }
-    return 0;
-}
 
-// The main class for the mod
-class $modify(PlayLayer) {
-    // This hook is called every frame
+    void setup() override {
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+
+        m_holdInput = TextInput::create(200.f, "Hold ms", "bigFont.fnt");
+        m_holdInput->setString(std::to_string(g_holdTimeMs));
+        m_holdInput->setPosition({ winSize.width / 2, winSize.height / 2 + 30 });
+        this->addChild(m_holdInput);
+
+        m_releaseInput = TextInput::create(200.f, "Release ms", "bigFont.fnt");
+        m_releaseInput->setString(std::to_string(g_releaseTimeMs));
+        m_releaseInput->setPosition({ winSize.width / 2, winSize.height / 2 - 10 });
+        this->addChild(m_releaseInput);
+    }
+
+    void FLAlert_Clicked(FLAlertLayer* layer, bool btn2) override {
+        if (!btn2) {
+            try {
+                g_holdTimeMs = std::stoi(m_holdInput->getString());
+                g_releaseTimeMs = std::stoi(m_releaseInput->getString());
+                log::info("Clickbot times updated: hold={}ms, release={}ms", g_holdTimeMs, g_releaseTimeMs);
+            } catch (...) {
+                log::error("Invalid input");
+            }
+        }
+        g_popupOpen = false;
+        this->removeFromParentAndCleanup(true);
+    }
+};
+
+class $modify(MyPlayLayer, PlayLayer) {
+    bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+        if (!PlayLayer::init(level, useReplay, dontCreateObjects)) return false;
+        g_timer = 0.f;
+        g_pressed = false;
+        return true;
+    }
+
     void update(float dt) {
         PlayLayer::update(dt);
 
-        // Check for a single press of the '\' key to toggle the clickbot
-        if (getSingleClick(KEY_OEM_5)) {
-            g_clicking = !g_clicking;
-            log::info("Clickbot {}", g_clicking ? "started" : "stopped");
+        if (!m_player1) return;
+
+        if (g_clicking) {
+            g_timer += dt * 1000.f; // ms
+
+            if (!g_pressed && g_timer >= g_releaseTimeMs) {
+                // press jump
+                this->pushButton(0, true); // 0 = jump button id
+                g_pressed = true;
+                g_timer = 0.f;
+            } else if (g_pressed && g_timer >= g_holdTimeMs) {
+                // release jump
+                this->releaseButton(0, true);
+                g_pressed = false;
+                g_timer = 0.f;
+            }
         }
     }
 };
 
-// This class is used to start the click thread when the mod is enabled
-class $modify(MyMod) {
-    void onEnable() {
-        log::info("Starting click thread...");
-        #ifdef GEODE_IS_WINDOWS
-        CreateThread(nullptr, 0, clickThread, nullptr, 0, nullptr);
-        #endif
+class $modify(MyDispatcher, CCKeyboardDispatcher) {
+    bool dispatchKeyboardMSG(enumKeyCodes key, bool down, bool idk) {
+        // Toggle clicking with \
+        if (key == enumKeyCodes::KEY_OEM_5 && down) {
+            g_clicking = !g_clicking;
+            log::info("Clickbot {}", g_clicking ? "started" : "stopped");
+        }
+
+        // Shift + \ opens popup
+        if (key == enumKeyCodes::KEY_OEM_5 && down && (GetKeyState(KEY_Shift) & 0x8000)) {
+            if (!g_popupOpen) {
+                g_popupOpen = true;
+                TimeEditPopup::create()->show();
+            }
+        }
+        return CCKeyboardDispatcher::dispatchKeyboardMSG(key, down, idk);
     }
 };
